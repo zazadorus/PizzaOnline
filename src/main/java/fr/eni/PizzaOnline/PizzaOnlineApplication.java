ec4ca15ba25c4e5ff0ad1c4ebb98b806ff14bf1d
zazadorus <isadora.vieytes@gmail.Com>
2023-12-11 10:26:45 +0100

    Initialisation du projet

---
+++ src/main/java/fr/eni/PizzaOnline/PizzaOnlineApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.PizzaOnline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/eni/PizzaOnline/PizzaOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.PizzaOnline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
