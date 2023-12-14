02922c571f9b74492257935af4d8cce25635ff2f
Hélias Johan <medaakdev@gmail.com>
2023-12-14 10:27:09 +0100

    Connexion front to back creation pizza

--- src/main/java/fr/eni/PizzaOnline/ihm/PizzaFormController.java
+++ src/main/java/fr/eni/PizzaOnline/ihm/PizzaFormController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import fr.eni.PizzaOnline.bll.BaseManager;
+import fr.eni.PizzaOnline.bll.CheeseManager;
+import fr.eni.PizzaOnline.bll.IngredientManager;
 import fr.eni.PizzaOnline.bll.PizzaManager;
 import fr.eni.PizzaOnline.bo.Pizza;
 import fr.eni.PizzaOnline.dal.PizzaDAO;
@@ -17,23 +20,31 @@ import jakarta.validation.Valid;
 @RequestMapping("/ajout-pizza")
 public class PizzaFormController {
 	@Autowired
-	//CheeseManager cheeseManager;
+	CheeseManager cheeseManager;
+	@Autowired
+	BaseManager baseManager;
+	@Autowired
 	PizzaManager pizzaManager;
-//	IngredientManager ingredientManager;
+	@Autowired
+	IngredientManager ingredientManager;
 	
 	@GetMapping
-	public String index( Pizza pizza, Model model) {
-	//	model.addAttribute("listeCheeses", cheeseManager.getAllCheeses());
-	//	model.addAttribute("listeIngredients", ingredientManager.getAllIngredients());
+	public String index(Pizza pizza, Model model) {
+		model.addAttribute("listeBases", baseManager.getAllBases());
+		model.addAttribute("listeCheeses", cheeseManager.getAllCheeses());
+		model.addAttribute("listeIngredients", ingredientManager.getAllIngredients());
 
 		return "pizza-form";
 	}
 	
 	@PostMapping
-	public String add(@Valid Pizza pizza,  Model model) {
+	public String add(@Valid Pizza pizza,  Model model, BindingResult errors) {
 
+		System.out.println("ICI TEST AJOUT PIZZA PERSO");
 		pizza.setAvailableOnSite(false);
-		pizza.setAvailableTakeAway(true);
+		pizza.setAvailableTakeAway(false);
+		pizza.getListCheese().forEach(System.out::println);
+		pizza.setPrice(pizzaManager.getPizzaPrice(pizza));
 		pizzaManager.addPizza(pizza);
 		return "redirect:/liste";
 }
