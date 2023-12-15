5323cc34a63a6c706b570d92242e99718caf906e
tiBoleb <thibolb@laposte.net>
2023-12-15 15:58:27 +0100

    Ajout du champ code de réduction

--- src/main/java/fr/eni/PizzaOnline/ihm/OrderController.java
+++ src/main/java/fr/eni/PizzaOnline/ihm/OrderController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import fr.eni.PizzaOnline.bll.CartManager;
 import fr.eni.PizzaOnline.bll.CustomerManager;
@@ -36,7 +37,7 @@ public class OrderController {
 	CustomerManager customerManager;
 	
 	@PostMapping
-	public String cart(HttpSession session, Model model) {
+	public String cart(HttpSession session, Model model, @RequestParam(name = "reductionCode") String reductionCode) {
 		
 		Cart cart;
 		PizzaOrder order;
@@ -51,14 +52,10 @@ public class OrderController {
 		} else {
 			// Sinon récupérer le panier de la session et la liste de pizza ajoutée au panier
 			cart = (Cart) session.getAttribute("cart");
+			System.out.println(reductionCode);
 			if (cart.getListPizza().size()>0) {
-				
-			pizzaList = cart.getListPizza();
-			order = new PizzaOrder(LocalDate.now(), "00h00", 
-					LocalDate.now(), "00h00", null, pizzaList, customerManager.getCustomerById(1));
+			order = pizzaOrderManager.setPizzaOrder(cart, customerManager.getCustomerById(1), reductionCode);
 			pizzaOrderManager.addPizzaOrder(order);
-			//PizzaOrder(LocalDate deliveryDate, String deliveryHour, LocalDate orderDate, String orderHour,
-			//ReductionCode reductionCode, List<Pizza> listPizza, Customer customer)
 			} else {
 				return "redirect:/commande";
 			}
@@ -72,33 +69,4 @@ public class OrderController {
 		
 	}
 	
-//	@PostMapping
-//	public String convertCartToOrder(HttpSession session, Model model) {
-////		if(errors.hasErrors()) {
-////			System.out.println("ERRRRRREUR");
-////			return "personne";
-////		}
-////		pizzaOrderManager.addContactMessage(contactMessage);
-////		session.setAttribute("contactMessage", contactMessage);
-//		System.out.println("YEAAAAAAAAAAAAAAAAAH HERE POST");
-//		Cart cart;
-//		List<Pizza> pizzaList;
-//				
-//		// Si l'attribut cart n'est pas enregistré en session, le créer
-//		if (session.getAttribute("cart") == null) {
-//			cart = new Cart();
-//			pizzaList = new ArrayList<Pizza>();
-//			session.setAttribute("cart", cart);
-//		} else {
-//			// Sinon récupérer le panier de la session et la liste de pizza ajoutée au panier
-//			cart = (Cart) session.getAttribute("cart");
-//			pizzaList = cart.getListPizza() != null ? cart.getListPizza() : new ArrayList<Pizza>();
-//		}
-//		
-//		model.addAttribute("cart", cart);
-//		model.addAttribute("listePizzas", pizzaManager.getAllPizzas());
-//		model.addAttribute("totalCart", cartManager.getTotalPrice(cart));
-//		
-//		return "redirect:/recap-commande";
-//	}
 }
